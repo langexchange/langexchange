1d1478cd62e22b582472cafdbdda974c19a517b5
Dinh Nhu Tan <nhutan2001@gmail.com>
2023-01-19 21:36:07 +0700

    Init folder structure

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,13 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import './index.css';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import { store } from "./stores/store";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import "./index.scss";
+import "antd/dist/reset.css";
 
-const container = document.getElementById('root')!;
+const container = document.getElementById("root")!;
 const root = createRoot(container);
 
 root.render(
