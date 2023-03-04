28053eed85db0ff624b5b0a83cd776be428a6860
Dinh Nhu Tan <nhutan2001@gmail.com>
2023-03-04 14:41:18 +0700

    integrate api for init basic info

--- src/ProtectedRoute.tsx
+++ src/ProtectedRoute.tsx
@@ -1,23 +1,49 @@
+import { message, Spin } from "antd";
+import { useEffect } from "react";
 import { Navigate, Outlet, useLocation } from "react-router-dom";
-import { selectCredentials } from "./features/auth/authSlice";
-import { useAppSelector } from "./hooks/hooks";
+import { selectCredentials, setCredentials } from "./features/auth/authSlice";
+import { useAppDispatch, useAppSelector } from "./hooks/hooks";
+import { useGetProfileMutation } from "./services/profile/profileServices";
 
 const ProtectedRoute = () => {
   const credentials = useAppSelector(selectCredentials);
+  const dispatch = useAppDispatch();
   const location = useLocation();
   const isInitial = location.pathname === "/initial";
+  const [getProfile, { isLoading }] = useGetProfileMutation();
 
-  if (credentials?.user && credentials?.token) {
-    if (!credentials.user.firstName && !isInitial) {
-      return <Navigate to="/initial" state={{ from: location }} replace />;
-    } else if (credentials.user.firstName && isInitial) {
-      return <Navigate to="/community" state={{ from: location }} replace />;
-    } else {
-      return <Outlet />;
+  useEffect(() => {
+    async function fetchProfile() {
+      try {
+        const profile = await getProfile(credentials.user.id).unwrap();
+        dispatch(
+          setCredentials({
+            ...credentials,
+            user: {
+              ...credentials.user,
+              firstName: profile.firstName,
+              lastName: profile.lastName,
+            },
+          })
+        );
+      } catch (error) {
+        console.log(error);
+        message.error("Oops! Something went wrong. Please try again.");
+      }
     }
-  } else {
+    fetchProfile();
+  }, []);
+
+  if (isLoading) return <Spin size="large" spinning={isLoading} />;
+
+  if (!credentials.user || !credentials.token)
     return <Navigate to="/sign-in" state={{ from: location }} replace />;
-  }
+  if (!credentials.user.firstName && !isInitial)
+    return <Navigate to="/initial" state={{ from: location }} replace />;
+  if (credentials.user.firstName && isInitial)
+    return <Navigate to="/community" state={{ from: location }} replace />;
+
+  return <Outlet />;
 };
 
 export default ProtectedRoute;
---
+++ src/services/profile/profileServices.ts
@@ -0,0 +1,64 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { RootState } from "../../stores/store";
+
+const baseUrl = process.env.REACT_APP_API_URL_ROOT;
+
+interface Language {
+  name: string;
+  level: number;
+}
+
+export interface ProfileRequest {
+  id: string;
+  body: {
+    nativeLanguage: Language;
+    targetLanguages: Language[];
+    userInfo: {
+      firstName: string;
+      lastName: string;
+      gender?: string | null;
+      introduction?: string;
+    };
+  };
+}
+
+interface GetProfileResponse {
+  firstName: string;
+  middleName: string;
+  lastName: string;
+  gender: string;
+  introduction: string;
+  nativeLanguage: Language;
+  targetLanguages: Language[];
+}
+
+export const profileApi = createApi({
+  reducerPath: "profileApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: baseUrl,
+    prepareHeaders: (headers, { getState }) => {
+      const token = (getState() as RootState).auth.token;
+      if (token) {
+        headers.set("authorization", `Bearer ${token}`);
+      }
+      return headers;
+    },
+  }),
+  endpoints: (builder) => ({
+    updateProfile: builder.mutation<null, ProfileRequest>({
+      query: (data) => ({
+        url: `api/users/${data.id}/fill-basic-information`,
+        method: "POST",
+        body: data.body,
+      }),
+    }),
+    getProfile: builder.mutation<GetProfileResponse, string>({
+      query: (id) => ({
+        url: `api/users/${id}/basic-information`,
+        method: "GET",
+      }),
+    }),
+  }),
+});
+
+export const { useUpdateProfileMutation, useGetProfileMutation } = profileApi;
