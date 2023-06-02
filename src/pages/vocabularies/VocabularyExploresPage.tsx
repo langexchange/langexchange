73da761c3b11b5d881b6c00ec9b975c03b60496e
Dinh Nhu Tan <nhutan2001@gmail.com>
2023-06-03 00:03:22 +0700

    add search and filter for partner, vocab explore

--- src/pages/vocabularies/VocabularyExploresPage.tsx
+++ src/pages/vocabularies/VocabularyExploresPage.tsx
@@ -1,14 +1,14 @@
 import { Input, Skeleton, Divider } from "antd";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 import FilterLine from "../../components/vocabularies/FilterLine";
 import VocabularySetList from "../../components/VocabularySetList";
 import {
   FilterVocabularySet,
   useGetSuggestVocabularySetsQuery,
+  VocabularySetDetail,
 } from "../../services/vocabulary/vocabularyService";
 
-const onSearch = (value: string) => console.log(value);
 const defaultFilters: FilterVocabularySet = {
   terms: [],
   defines: [],
@@ -17,11 +17,99 @@ const defaultFilters: FilterVocabularySet = {
 const VocabularyExploresPage: React.FC = () => {
   const [t] = useTranslation(["commons"]);
   const [filters, setFilters] = useState<FilterVocabularySet>(defaultFilters);
-  const { data, isLoading } = useGetSuggestVocabularySetsQuery(filters, {
-    refetchOnMountOrArgChange: true,
-  });
+  const { data, isLoading, isFetching } = useGetSuggestVocabularySetsQuery(
+    filters,
+    {
+      refetchOnMountOrArgChange: true,
+    }
+  );
+  const [displayedData, setDisplayedData] = useState<VocabularySetDetail[]>([]);
+
+  useEffect(() => {
+    if (data) {
+      setDisplayedData(data);
+    }
+  }, [data, isLoading]);
+
+  const onSearch = (value: string) => {
+    if (!data) return;
+
+    if (!value) {
+      setDisplayedData(data);
+      return;
+    }
+
+    const newData = data?.filter((item) => {
+      if (
+        item.userInfo.firstName?.toLowerCase().includes(value.toLowerCase()) ||
+        item.userInfo.lastName?.toLowerCase().includes(value.toLowerCase())
+      )
+        return true;
+      return item.vocabularyPackageDtos.some((vocabularyPackage) => {
+        if (
+          vocabularyPackage.title
+            ?.toLowerCase()
+            .includes(value.toLowerCase()) ||
+          vocabularyPackage.description
+            ?.toLowerCase()
+            .includes(value.toLowerCase())
+        )
+          return true;
+      });
+    });
+
+    const finalData = newData.map((item) => {
+      return {
+        ...item,
+        vocabularyPackageDtos: item.vocabularyPackageDtos.filter(
+          (vocabularyPackage) => {
+            if (
+              vocabularyPackage.title
+                ?.toLowerCase()
+                .includes(value.toLowerCase()) ||
+              vocabularyPackage.description
+                ?.toLowerCase()
+                .includes(value.toLowerCase())
+            )
+              return true;
+          }
+        ),
+      };
+    });
+    setDisplayedData(finalData);
+  };
 
   const resetFilters = () => setFilters(defaultFilters);
+  useEffect(() => {
+    console.log(filters);
+    if (!filters) return;
+    if (filters.terms?.length === 0 && filters.defines?.length === 0) return;
+    if (!data) return;
+
+    const newData = data?.map((item) => {
+      return {
+        ...item,
+        vocabularyPackageDtos: item.vocabularyPackageDtos.filter(
+          (vocabularyPackage) => {
+            if (
+              filters.terms?.some((term) =>
+                vocabularyPackage.termLocale
+                  ?.toLowerCase()
+                  .includes(term.toLowerCase())
+              ) ||
+              filters.defines?.some((define) =>
+                vocabularyPackage.defineLocale
+                  ?.toLowerCase()
+                  .includes(define.toLowerCase())
+              )
+            )
+              return true;
+          }
+        ),
+      };
+    });
+    setDisplayedData(newData);
+  }, [isFetching]);
 
   return (
     <div className="pt-4 pb-5">
@@ -36,7 +124,7 @@ const VocabularyExploresPage: React.FC = () => {
       </div>
       <Divider />
       <Skeleton loading={isLoading} active>
-        <VocabularySetList data={data} />
+        <VocabularySetList data={displayedData} />
       </Skeleton>
     </div>
   );
